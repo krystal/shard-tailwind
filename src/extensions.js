302a29955294ab6729d97a577de7725fdb52f0a0
Paul Sturgess <paul.sturgess@krystal.uk>
2024-08-12 10:29:51 +0100

    feat: add xs screen breakpoint

--- src/extensions.js
+++ src/extensions.js
@@ -1,7 +1,13 @@
+const { screens } = require("tailwindcss/defaultTheme");
+
 const spacing = {
   4.5: "1.125.rem",
 };
 
 module.exports = {
+  screens: {
+    xs: "480px",
+    ...screens,
+  },
   spacing,
 };
